4cdccec2da5c4aa7e3ed739a9b2cc0221bca8ef1
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-03-12 12:46:56 -0500

    chore(Root): start project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD9jOxl6SbUqfCbxNDIK7-E3JA2YE2Kykk',
+  authDomain: 'netflix-clone-b4e95.firebaseapp.com',
+  projectId: 'netflix-clone-b4e95',
+  storageBucket: 'netflix-clone-b4e95.appspot.com',
+  messagingSenderId: '182988107370',
+  appId: '1:182988107370:web:b1307d26f957694b2ba413'
+};
