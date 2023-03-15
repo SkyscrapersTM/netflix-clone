947e6c535873511fa7de9fcaf3e35bb3eed5d492
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-03-14 20:12:43 -0500

    feat(Root): add Row component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import './Home.css'
+import requests from '../services/requests'
 import Nav from '../components/Nav/Nav'
 import Banner from '../components/Banner/Banner'
+import Row from '../components/Row/Row'
 
 const Home = () => {
   return (
     <div className='home'>
       <Nav />
       <Banner />
-      {/* Row */}
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomaceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
