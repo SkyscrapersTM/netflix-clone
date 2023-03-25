251ef53a217fbf20897ba660b72d79283302bead
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-03-25 17:21:43 -0500

    feat(Root): Added new routes for in Nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Nav.css'
 
 const Nav = () => {
   const [show, setShow] = useState(false)
-
+  const navigate = useNavigate()
   const transitionNavbar = () => setShow(window.scrollY > 100)
 
   useEffect(() => {
@@ -15,11 +16,13 @@ const Nav = () => {
     <div className={`nav ${show && 'nav__black'}`}>
       <div className='nav__contents'>
         <img
+          onClick={() => navigate('/')}
           className='nav__logo'
           src='https://1000marcas.net/wp-content/uploads/2020/01/Logo-Netflix.png'
           alt='netflix logo'
         />
         <img
+          onClick={() => navigate('/profile')}
           className='nav__avatar'
           src='https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png'
           alt='netflix avatar'
